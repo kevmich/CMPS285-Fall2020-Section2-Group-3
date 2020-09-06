d5797d80ccf27673428fd59da52f93f66c8ec3f7
jaredmartin5 <jaredm776@cox.net>
2020-09-06 12:50:44 -0500

    Added login page

--- KitchenVideoSystem/ClientApp/src/App.js
+++ KitchenVideoSystem/ClientApp/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import Login  from './components/Login';
+
 
 import './custom.css'
 
@@ -12,6 +14,8 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+
       </Layout>
     );
   }
---
+++ KitchenVideoSystem/ClientApp/src/components/Login.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export default class Login extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> Login </h1>
+            </div>
+        );
+    }
+}
