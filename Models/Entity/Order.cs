b4e33319fdc5334da56029adc7558777122e2bc0
calebmartin2 <calebm776@cox.net>
2020-09-10 19:23:05 -0500

    Added connection to database

--- Models/Entity/Order.cs
+++ Models/Entity/Order.cs
@@ -5,9 +5,12 @@ namespace Models
 {
     public class Order
     {
-        public int[] TimeOrdered { get; set; }
-        public int OrderNumber { get; set; }
-        public int OrderContents { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+        //public int[] TimeOrdered { get; set; }
+        //public int OrderNumber { get; set; }
+        //public int OrderContents { get; set; }
 
     }
 }
--- Repository/OrderRepository.cs
+++ Repository/OrderRepository.cs
@@ -1,10 +1,14 @@
-﻿using Models;
+﻿using Dapper;
+using Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace Repository
 {
+    //Server=SQLEXPRESS;Database=KitchenVideoSystemDb;Integrated Security=true;
     public interface IOrderRepository
     {
         Order[] GetOrders();
@@ -13,14 +17,23 @@ namespace Repository
     {
         public Order[] GetOrders()
         {
+            var connectionString = @"Server=DESKTOP-V733GDC\SQLEXPRESS;Database=KitchenVideoSystemDb;Integrated Security=true;";
+            
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Orders";
+                var orders = connection.Query<Order>(sql).ToArray();
+                return orders;
+            }
+
             // This is where you use Dapper
-            int[] TimeOrderedArr = new int[] { 32, 34, 224 };
+            //int[] TimeOrderedArr = new int[] { 32, 34, 224 };
 
-            var order1 = new Order { OrderNumber = 1, OrderContents = 2, TimeOrdered = TimeOrderedArr};
+            //var order1 = new Order { OrderNumber = 1, OrderContents = 2, TimeOrdered = TimeOrderedArr};
 
 
-            Order[] retVal = { order1 };
-            return retVal;
+            //Order[] retVal = { order1 };
+            //return retVal;
         }
 
         public T GetStuff<T>(T item)
