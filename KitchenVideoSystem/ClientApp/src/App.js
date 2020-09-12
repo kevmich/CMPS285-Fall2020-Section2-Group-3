aa74c100b863c9a32f489ffc2ac004586a4e7e94
jaredmartin5 <jaredm776@cox.net>
2020-09-11 19:12:08 -0500

    Made cashier screen

--- KitchenVideoSystem/ClientApp/src/App.js
+++ KitchenVideoSystem/ClientApp/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import Login  from './components/Login';
+import  Login  from './components/Login';
+import  CashierScreen  from './components/CashierScreen';
 
 
 import './custom.css'
@@ -15,7 +16,7 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
-
+        <Route exact path= '/CashierScreen' component={CashierScreen} />
       </Layout>
     );
   }
---
+++ KitchenVideoSystem/ClientApp/src/components/CashierScreen.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export default class CashierScreen extends Component {
+    render() {
+        return (
+            <div>
+              <h1> Cash </h1>
+            </div>
+        );
+    }
+}
