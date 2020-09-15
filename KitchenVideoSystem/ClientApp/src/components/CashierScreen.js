bd827dd54362cb123f9f01b1d7d5eb7b5dfb700b
jaredmartin5 <jaredm776@cox.net>
2020-09-14 19:24:50 -0500

    Deleted the unused css commits and named the drink buttons.

--- KitchenVideoSystem/ClientApp/src/components/CashierScreen.js
+++ KitchenVideoSystem/ClientApp/src/components/CashierScreen.js
@@ -11,18 +11,18 @@ export default class CashierScreen extends Component {
                     <button class="Button"> Hamburger </button>
                     <button class="Button"> Cheeseburger </button>
                     <button class="Button"> Chicken Nuggets </button>
+                    <button class="Button"> Hot dog </button>
                     <button class="Button"> Pretzel </button>
-                    <button class="Button"> French Fries </button>
                     <button class="Button"> French Fries </button>
                 </div>
                 <div class="MenuItems">
                     <h1 class="food"> Drinks </h1>
-                    <button class="Button"> Hamburger </button>
-                    <button class="Button"> Cheeseburger </button>
-                    <button class="Button"> Chicken Nuggets </button>
-                    <button class="Button"> Pretzel </button>
-                    <button class="Button"> French Fries </button>
-                    <button class="Button"> French Fries </button>
+                    <button class="Button"> Sprite </button>
+                    <button class="Button"> Coca-Cola </button>
+                    <button class="Button"> Fanta Orange </button>
+                    <button class="Button"> Dr Pepper </button>
+                    <button class="Button"> Lemonade </button>
+                    <button class="Button"> Water </button>
                 </div>
                 <div class="CurrentOrder MenuItems">
                     <p>CURRENT ORDER</p>
