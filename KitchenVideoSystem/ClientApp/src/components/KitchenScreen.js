ae91f3d7a07009ef91e13ab44315a5f16013ba74
jaredmartin5 <jaredm776@cox.net>
2020-09-15 15:30:20 -0500

    Added a kitchen screen.

---
+++ KitchenVideoSystem/ClientApp/src/components/KitchenScreen.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export default class KitchenScreen extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> this is a test </h1>
+            </div>
+        );
+    }
+}
