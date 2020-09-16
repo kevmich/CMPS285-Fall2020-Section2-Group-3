103eb021b75d27f2be3ba577523d59683e95edfe
John Juban <john.juban@gmail.com>
2020-09-15 21:52:50 -0500

    Created database tables for OrderItems and Orders

---
+++ KitchenDatabase/dbo/Tables/OrderItems.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[OrderItems] (
+    [Id]   INT          NOT NULL,
+    [Name] VARCHAR (50) NOT NULL,
+    [Type] VARCHAR (20) NOT NULL,
+    CHECK ([Type]='drink' OR [Type]='food')
+);
+
