6f8a4a9618847b98b8515540d160505176ac0054
Nicholas Manchester <nicholas.manchester@selu.edu>
2020-09-17 18:15:56 -0500

    Added Employee Class

---
+++ Models/Entity/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Passwprd { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateHired { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        //public string SSN { get; set; } Should this be public?
+        public int Role { get; set; }
+    }
+}
