f1cabdef5d96ccaf06d322c828e0de54dd14214e
Nicholas Manchester <nicholas.manchester@selu.edu>
2020-09-17 19:00:18 -0500

    Fixed a Typo

--- Models/Entity/Employee.cs
+++ Models/Entity/Employee.cs
@@ -6,7 +6,7 @@ namespace Models
     public class Employee
     {
         public int Id { get; set; }
-        public string Passwprd { get; set; }
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateHired { get; set; }
