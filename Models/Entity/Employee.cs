aa1f24fc8b183c8526db0e3e26f9da283ecd87d7
Nicholas Manchester <nicholas.manchester@selu.edu>
2020-09-17 22:48:20 -0500

    Removed unneeded comment

--- Models/Entity/Employee.cs
+++ Models/Entity/Employee.cs
@@ -12,7 +12,6 @@ namespace Models
         public DateTime DateHired { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        //public string SSN { get; set; } Should this be public?
         public int Role { get; set; }
     }
 }
