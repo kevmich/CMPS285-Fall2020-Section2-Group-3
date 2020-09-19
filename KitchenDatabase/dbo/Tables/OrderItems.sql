1466507277f1c0b99eebf2c884c9972e2bbef60e
calebmartin2 <calebm776@cox.net>
2020-09-19 00:41:50 -0500

    Fixed Identity

--- KitchenDatabase/dbo/Tables/OrderItems.sql
+++ KitchenDatabase/dbo/Tables/OrderItems.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[OrderItems] (
-    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Id]   INT          NOT NULL,
     [Name] VARCHAR (50) NOT NULL,
     [Type] INT          NOT NULL,
     CONSTRAINT [PK_OrderItems] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
