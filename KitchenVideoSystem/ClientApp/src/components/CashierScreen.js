f65d18f62bb36c4bcea3e809bc5fc8ac7c731880
jaredmartin5 <jaredm776@cox.net>
2020-09-23 22:12:42 -0500

    Imported uuid, added some logos, and added button functionality.

--- KitchenVideoSystem/ClientApp/src/components/CashierScreen.js
+++ KitchenVideoSystem/ClientApp/src/components/CashierScreen.js
@@ -1,11 +1,13 @@
 ﻿import React, { Component } from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import './CashierScreen.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHamburger } from '@fortawesome/free-solid-svg-icons'
+import { faHotdog } from '@fortawesome/free-solid-svg-icons'
 import cheeseburgerIcon from '../content/cheeseburger-solid.svg';
 const hamburgerIcon = <FontAwesomeIcon icon={faHamburger} />
-
+const hotdogIcon = <FontAwesomeIcon icon={faHotdog} />
 
 export default class CashierScreen extends Component {
 
@@ -13,9 +15,14 @@ export default class CashierScreen extends Component {
         super(props);
         this.onClick = this.onClick.bind(this);
         this.state = {
-            OrderNumber: "9cdc7364-90d8-4a1f-a7ae-bb3e85681ec0",
+            OrderNumber: uuidv4()
         };
     }
+    componentDidMount() {
+        console.log(uuidv4());
+    }
+
+
 
     onClick(id, size) {
         axios({
@@ -40,10 +47,10 @@ export default class CashierScreen extends Component {
                     <b class="food"> Food </b>
                     <button onClick={this.onClick(1, 0)} class="Button">{hamburgerIcon}&nbsp;Hamburger</button>
                     <button onClick={this.onClick(2, 0)} class="Button"><img src={cheeseburgerIcon} height="47px"/>&nbsp;Cheeseburger </button>
-                    <button class="Button"> Chicken Nuggets </button>
-                    <button class="Button"> Corn Dog </button>
-                    <button class="Button"> Hot Dog </button>
-                    <button class="Button"> Pretzel </button>
+                    <button onClick={this.onClick(3, 0)} class="Button"> Chicken Nuggets </button>
+                    <button onClick={this.onClick(4, 0)} class="Button"> Corn Dog </button>
+                    <button onClick={this.onClick(5, 0)} class="Button"> {hotdogIcon}&nbsp;Hot Dog </button>
+                    <button onClick={this.onClick(6, 0)} class="Button"> Pretzel </button>
                 </div>
 
 
@@ -57,39 +64,39 @@ export default class CashierScreen extends Component {
                         </tr>
                         <tr>
                             <td class="Drinks"> Sprite </td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(7, 1)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(7, 2)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(7, 3)} class="DButton"> </ button></td>
                         </tr>
                         <tr>
                             <td class="Drinks"> Coca-Cola </td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(8, 1)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(8, 2)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(8, 3)} class="DButton"> </ button></td>
                         </tr>
                         <tr>
                             <td class="Drinks"> Fanta Orange </td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(9, 1)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(9, 2)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(9, 3)} class="DButton"> </ button></td>
                         </tr>
                         <tr>
                             <td class="Drinks"> Dr Pepper </td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(10, 1)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(10, 2)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(10, 3)} class="DButton"> </ button></td>
                         </tr>
                         <tr>
                             <td class="Drinks"> Lemonade </td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(11, 1)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(11, 2)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(11, 3)} class="DButton"> </ button></td>
                         </tr>
                         <tr>
                             <td class="Drinks"> Water </td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
-                            <td>< button class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(11, 1)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(11, 2)} class="DButton"> </ button></td>
+                            <td>< button onClick={this.onClick(11, 3)} class="DButton"> </ button></td>
                         </tr>
 
 
