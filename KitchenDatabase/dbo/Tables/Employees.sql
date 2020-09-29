19d48e3a381261ce137bdc331bf294c8d24fb499
John Juban <john.juban@gmail.com>
2020-09-28 23:28:44 -0500

    Added Employees table and EmployeeRoles table. Changed EmployeeId in Employee.cs to int

---
+++ KitchenDatabase/dbo/Tables/Employees.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeId] INT          NOT NULL,
+    [FirstName]  VARCHAR (75) NOT NULL,
+    [LastName]   VARCHAR (75) NOT NULL,
+    [Role]       INT          IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [FK_Employees_EmployeeRoles] FOREIGN KEY ([Role]) REFERENCES [dbo].[EmployeeRoles] ([Id])
+);
+
--- Models/Entity/Employee.cs
+++ Models/Entity/Employee.cs
@@ -5,7 +5,7 @@ namespace Models
 {
     public class Employee
     {
-        public string EmployeeId { get; set; }
+        public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
