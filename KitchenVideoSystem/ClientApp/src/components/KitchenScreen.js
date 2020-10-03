f82eafcf0a37f7d76bc8f68308d4f051b9e1fe07
jaredmartin5 <jaredm776@cox.net>
2020-10-03 18:24:36 -0500

    Drink logos work not food kitchen.

--- KitchenVideoSystem/ClientApp/src/components/KitchenScreen.js
+++ KitchenVideoSystem/ClientApp/src/components/KitchenScreen.js
@@ -1,6 +1,15 @@
 ﻿import React, { Component } from 'react';
 import './KitchenScreen.css'
 import axios from 'axios';
+import hamburgerIcon from '../content/hamburger-solid.svg';
+import cheeseburgerIcon from '../content/cheeseburger-solid.svg';
+import nuggiesIcon from '../content/nuggets.svg';
+import corndogIcon from '../content/corn-dog.svg';
+import pretzelIcon from '../content/pretzel.svg';
+import smalldrinkIcon from '../content/drink-small.svg';
+import mediumdrinkIcon from '../content/drink-medium.svg';
+import largedrinkIcon from '../content/drink-large.svg';
+import hotdogIcon from '../content/hotdog-solid.svg'
 import Clock from 'react-digital-clock'
 var _ = require('lodash');
 
@@ -37,6 +46,40 @@ export default class KitchenScreen extends Component {
         clearInterval(this.interval);
     }
 
+    iconSwitch(param, param2) {
+        switch (param) {
+            case 1:
+                return <img src={hamburgerIcon} class="CurrentIcon" height="47px" />
+            case 2:
+                return <img src={cheeseburgerIcon} class="CurrentIcon" height="47px" />;
+            case 3:
+                return <img src={nuggiesIcon} class="CurrentIcon" height="47px" />;
+            case 4:
+                return <img src={corndogIcon} class="CurrentIcon" height="47px" />;
+            case 5:
+                return <img src={hotdogIcon} class="CurrentIcon" height="47px" />;
+            case 6:
+                return <img src={pretzelIcon} class="CurrentIcon" height="47px" />;
+            case 7:
+                this.iconSwitchDrink(param2);
+            default:
+                return;
+        }
+    }
+
+    iconSwitchDrink(param) {
+        switch (param) {
+            case 1:
+                return <img src={smalldrinkIcon} class="CurrentIcon" width="40px" />;
+            case 2:
+                return <img src={mediumdrinkIcon} class="CurrentIcon" width="40px" />;
+            case 3:
+                return <img src={largedrinkIcon} class="CurrentIcon" width="40px" />;
+            default:
+                return;
+        }
+    }
+
     sizeSwitch(param) {
         switch (param) {
             case 1:
@@ -70,7 +113,7 @@ export default class KitchenScreen extends Component {
             return (
                 <div class="GroupOrder" id={value[0].isComplete ? "completeOrders" : "incompleteOrders"} key={key} onClick={() => this.serveOrder(key, value[0].isComplete)}>
                     {value.map((Order) => (
-                        <p>{this.sizeSwitch(Order.size)}{Order.name} <br /> </p>
+                        <p>{this.iconSwitch(Order.orderItemId)}{this.iconSwitchDrink(Order.size)}{this.sizeSwitch(Order.size)} {Order.name} <br /> </p>
                     ))}
                 </div>
             )
