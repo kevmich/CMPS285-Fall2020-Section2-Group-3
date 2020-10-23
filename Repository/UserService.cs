85a67281624b76a28d3fa18f7a1136e06b198ff2
calebmartin2 <calebm776@cox.net>
2020-10-23 13:34:50 -0500

    Fixed crash on login with invalid username

--- Repository/UserService.cs
+++ Repository/UserService.cs
@@ -1,45 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Models;
+﻿using Dapper;
+using EasyEncryption;
+using Microsoft.Extensions.Options;
 using Models.Domain;
 using Models.Entity;
-using Dapper;
-using Microsoft.Extensions.Options;
+using System;
 using System.Data.SqlClient;
-using EasyEncryption;
-
 
 namespace TokenBasedAuth.Services
-    {
-public interface IUserService
+{
+    public interface IUserService
     {
         bool IsValidUser(UserModel user);
     }
+
     public class UserService : IUserService
     {
+        private string _connectionString = "";
 
-    private string _connectionString = "";
-
-    public UserService(IOptions<ConnectionStringsOptions> connectionStringsOptions)
-    {
-        //inject appsettings
-        _connectionString = connectionStringsOptions.Value.KitchenVideoSystemDb;
-    }
-
-    public bool IsValidUser(UserModel user)
-    {
-        using (var connection = new SqlConnection(_connectionString))
+        public UserService(IOptions<ConnectionStringsOptions> connectionStringsOptions)
         {
-            //var parameter = new { Username = user.Username};
-            var sql = "SELECT Users.Password FROM Users WHERE Username = '" + user.Username + "'";
-            var comparePass = connection.QuerySingle<String>(sql);
-            if (comparePass == SHA.ComputeSHA256Hash(user.Password))
-                return true;
-            else
-                return false;
-
+            //inject appsettings
+            _connectionString = connectionStringsOptions.Value.KitchenVideoSystemDb;
         }
-    }
+
+        public bool IsValidUser(UserModel user)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    //var parameter = new { Username = user.Username};
+                    var sql = "SELECT Users.Password FROM Users WHERE Username = '" + user.Username + "'";
+                    var comparePass = connection.QuerySingle<String>(sql);
+                    if (comparePass == SHA.ComputeSHA256Hash(user.Password))
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
