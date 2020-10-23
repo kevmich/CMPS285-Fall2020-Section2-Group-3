60991f19a7ae0f5cd4afd1a6e7f42fbe9d7e0799
Nicholas Manchester <nicholas.manchester@selu.edu>
2020-10-23 15:20:54 -0500

    Deleted Unused Employee Content

--- KitchenDatabase/dbo/Tables/EmployeeRoles.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[EmployeeRoles] (
-    [Id]   INT          IDENTITY (1, 1) NOT NULL,
-    [Role] VARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_EmployeeRoles] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
