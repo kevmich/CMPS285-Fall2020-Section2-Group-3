1f1c08742c26f401a93e89c9b153d011a64eaf4e
John Juban <john.juban@gmail.com>
2020-10-27 16:12:52 -0500

    Added Id column to Userswith Primary key.

--- KitchenDatabase/dbo/Tables/Users.sql
+++ KitchenDatabase/dbo/Tables/Users.sql
@@ -1,5 +1,9 @@
 ﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT          NOT NULL,
     [Username] VARCHAR (50) NULL,
-    [Password] VARCHAR (64) NULL
+    [Password] VARCHAR (64) NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
