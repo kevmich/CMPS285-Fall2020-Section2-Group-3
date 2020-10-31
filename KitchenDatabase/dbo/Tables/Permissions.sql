9781df9a5c220d2295c6c74300f0e52a45add92c
calebmartin2 <calebm776@cox.net>
2020-10-30 20:14:08 -0500

    Schema compare

---
+++ KitchenDatabase/dbo/Tables/Permissions.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Permissions] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [PermissionId] INT          NOT NULL,
+    [Name]         VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Permissions] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ KitchenDatabase/dbo/Tables/UsersPermissions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UsersPermissions] (
+    [UserId]       INT NOT NULL,
+    [PermissionId] INT NOT NULL,
+    CONSTRAINT [PK_UsersPermissions] PRIMARY KEY CLUSTERED ([UserId] ASC, [PermissionId] ASC),
+    CONSTRAINT [FK_UsersPermissions_Permissions] FOREIGN KEY ([PermissionId]) REFERENCES [dbo].[Permissions] ([Id]),
+    CONSTRAINT [FK_UsersPermissions_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
+
