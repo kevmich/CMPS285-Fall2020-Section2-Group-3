d8b73338c12ebc0071613ef3bc587a18470074ab
jaredmartin5 <jaredm776@cox.net>
2020-11-03 12:10:51 -0600

    users show on screen

--- KitchenVideoSystem/ClientApp/src/components/Admin.js
+++ KitchenVideoSystem/ClientApp/src/components/Admin.js
@@ -1,7 +1,27 @@
 ﻿import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import "./Admin.css";
 export default class Admin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Users: [],
+        };
+    }
+
+    GetAllUsers() {
+        axios.get('api/user/GetAllUsers')
+            .then((response) => {
+                this.setState({
+                    Users: response.data
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.GetAllUsers();
+    }
 
     render() {
         return (
@@ -9,12 +29,17 @@ export default class Admin extends Component {
                 <h1 className="title"> Manage Users </h1>
 
                 <Link to="./admin/adduser">
-                <button class="addButton">Add User</button>
+                    <button class="addButton">Add User</button>
                 </Link>
 
                 <Link to="./">
                     <button class="backButton">back</button>
                 </Link>
+                {this.state.Users.map((user) => (
+                    <div>
+                        <p> {user} </p>
+                    </div>
+                ))}
 
             </div>
         );
