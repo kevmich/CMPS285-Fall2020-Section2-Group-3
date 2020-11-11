9114f9faa6c850d671352c24551a81f6dc9acbf4
calebmartin2 <calebm776@cox.net>
2020-11-11 02:20:29 -0600

    Simple log returning works

--- KitchenVideoSystem/ClientApp/src/components/Log.js
+++ KitchenVideoSystem/ClientApp/src/components/Log.js
@@ -9,15 +9,22 @@ class Log extends Component {
         super(props);
 
         this.state = {
-            LogData: []
+            LogData: [],
+            date: ''
         };
+
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.GetLog = this.GetLog.bind(this);
+
     }
 
-    componentDidMount() {
+    GetLog() {
         axios({
             method: 'post',
             headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + sessionStorage.getItem('token') },
-            url: '/api/log/GetDayLog/' + '2020-11-11',
+            url: '/api/log/GetDayLog/' + this.state.date,
             data: 5
         }).then((response) => {
             console.log(response.data)
@@ -29,17 +36,33 @@ class Log extends Component {
 
     }
 
+    handleChange(event) {
+        this.setState({ date: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.date);
+        this.GetLog();
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
                 <h1> hello! </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Get orders from day:
+                    <input type="date" id="birthday" name="senddate" value={this.state.date} onChange={this.handleChange}></input>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
                 {
                     this.state.LogData.map((data) => (
                         <p>{data.name}, {data.size}, {data.dateStarted}</p>
 
                     ))
                 }
-
             </div>
         );
     }
